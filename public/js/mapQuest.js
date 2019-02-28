2e93010fb17f5c09a1320c3caf06c65b3d2adb3f
Gillian Grennan <ggrennan@ucsd.edu>
2019-02-28 10:42:02 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'InzLFkbyYhnAdUK6pNo6sQgVmcj6ME8F';
+
+	var map = L.mapquest.map('map', {
+		center: [32.8788, -117.2359],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+
+
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
+
 }
